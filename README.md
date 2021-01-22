51e015c0628eabbe3a47ff017fb6eabc84e4401d
Declan Scott <datoneguy246@gmail.com>
2021-01-22 11:41:26 -0500

    Github Pages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Discourse
 
-A chatroom site
+A chatroom site made for a school project:
 Currently live at: https://datoneguy246.github.io/Discourse/
--- login.php
+++ login.php
@@ -37,7 +37,7 @@
 <?php include "html/header.html" ?>
 <div>
     <h1>Log In on Discourse</h1>
-    <form action="/index.php" method="POST">
+    <form action="/login.php" method="POST">
         <input name="username" placeholder="Username" required value="<?php print($_POST["username"]) ?>" />
         <input name="password" placeholder="Password" type="password" required />
         <input type="submit" value="Log In" />
