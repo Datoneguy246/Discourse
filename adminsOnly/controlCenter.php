1a9aa9a3a96e8c4409c2e928eecad42c1362d476
Declan Scott <datoneguy246@gmail.com>
2021-02-01 11:45:10 -0500

    new protection system

--- adminsOnly/controlCenter.php
+++ adminsOnly/controlCenter.php
@@ -2,13 +2,19 @@
 <p>If you got here without our permission, may god have mercy on us all...</p>
 <hr>
 <form name="form" action="./controlCenter.php" method="POST">
-  <input type="text" name="query" id="query">
+  Query: <input type="text" name="query" id="query"><br>
+  Password: <input type="text" name="password" id="password">
   <button type="submit">Query</button>
   <p>There is no undoing these commands, be careful</p>
 </form>
 <hr>
 
 <?php
+    $password = $_POST['password'];
+    if($password != "Py6qHmwp9H9abKap7vHLE4ZVnJAMRzw4bVeabSgjCDreKCVf3z3tWPCYRnpGWcmnHDY72mE5bWRB4NDrQCE2t2CTDCemqZwC3jcZacTxsCGxwuZmgCyXYK3536KtZkUwKJurRvuGJVuhFeDjX2rzwJRKBneVtC82YBswyUXzcZZ2mgDWvxFHeE74HWEzLc5tvuZBAhs77z2bY6q9u3rmcTVeTjQQmUdd8ftBhwXbK6yLGnEykNN4Yd6JLLjk6k3z")
+    {
+        die("ACCESS DENIED");
+    }
     $db = new SQLite3("../backend_php/users.db");
 
     if ($_SERVER["REQUEST_METHOD"] === "POST")
