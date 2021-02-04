47542dc470d1df7ac753a8de2fec1e86dd0f7f94
stefano <stetasso05@gmail.com>
2021-02-04 11:57:36 -0500

    style changes

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
         <h1>A Newman Student<br/> Built Chatting<br/> Website</h1>
     </div>
     <hr>
-    <div class="made-by">
-    <h1>Made By</h1> 
-    </div>
-    <p>I don't know what we should add, but we have to add something</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    <p>Filler</p>
-    
+    <div class="made-by-page">
+        <div class="made-by">
+        <h1>Made By</h1> 
+        </div>
+
+    <p> filter </p>
+    <p> filter </p>
+    <p> filter </p>
+    <p> filter </p>
+    <p> filter </p>
+    <p> filter </p>    
 </main>
 <?php include "html/footer.html" ?>
+</div>
