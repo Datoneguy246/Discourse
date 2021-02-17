0c7caab9711a82c0448df5698dccf3f8340d22d3
stefano <stetasso05@gmail.com>
2021-02-17 11:47:54 -0500

    style chnages

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php include "html/header.html" ?>
-<main>
-    <div class="intro">
+<main class="intro d-flex align-items-center justify-content-center">
         <div>
-            <h1>A chatting site<br/> built by Newman students<br/> for Newman students</h1>
+            <h1 class="text-center">A chatting site<br/> built by Newman students<br/> for Newman students</h1>
             <div class="row justify-content-center">
-            <a class="btn btn-light col-6" href="/register.php">Register Now</a>
+            <a class="btn btn-primary col-6 text-white" href="/register.php">Register Now</a>
             </div>    
         </div>
-    </div>
-    <hr>
-    <div class="made-by-page">
-        <div class="made-by">
-        <h1>Made By</h1> 
-        </div>
-
-    <p> filter </p>
-    <p> filter </p>
-    <p> filter </p>
-    <p> filter </p>
-    <p> filter </p>
-    <p> filter </p>    
 </main>
 <?php include "html/footer.html" ?>
 </div>
